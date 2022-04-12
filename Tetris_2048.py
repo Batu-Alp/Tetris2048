5433f44e06593377ee37b27939c2ebd5e3cef84d
Batu Alp <68554767+Batu-Alp@users.noreply.github.com>
2022-04-12 22:47:09 +0300

    Add files via upload

---
+++ Tetris_2048.py
@@ -0,0 +1,310 @@
+import lib.stddraw as stddraw  # stddraw is used as a basic graphics library
+from lib.picture import Picture  # used for displaying images
+from lib.color import Color  # used for coloring the game menu
+import os  # the os module is used for file and directory operations
+from game_grid import GameGrid # the class for modeling the game grid
+from tetromino import Tetromino # the class for modeling the tetrominoes
+import random # used for creating tetrominoes with random types/shapes
+import pygame
+
+# MAIN FUNCTION OF THE PROGRAM
+#-------------------------------------------------------------------------------
+# Main function where this program starts execution
+
+
+def draw_canvas():
+   
+   # set the dimensions of the game grid
+   global grid_h, grid_w, main_w
+   grid_h, grid_w = 20, 12
+   main_w = 18
+   # set the size of the drawing canvas
+   canvas_h, canvas_w = 40 * grid_h, 60 * grid_w
+   stddraw.setCanvasSize(canvas_w, canvas_h)
+   # set the scale of the coordinate system
+   stddraw.setXscale(-0.5, grid_w + 4.5)
+   stddraw.setYscale(-0.5, grid_h - 0.5)
+
+
+   global  paused, game_over, restart
+
+   paused = False
+   game_over = False
+   restart = False
+
+   # display a simple menu before opening the game
+   display_game_menu(grid_h, main_w,paused, game_over, restart)
+   start(grid_h, grid_w, main_w, paused, game_over, restart)   
+
+def start(grid_h, grid_w, main_w, paused, game_over, restart ):
+   
+ 
+   global grid
+   grid = GameGrid(grid_h, grid_w)
+
+   change_speed(grid_h, main_w) 
+
+   current_tetromino = create_tetromino(grid_h, grid_w)
+   grid.next_tetromino = create_tetromino(grid_h, grid_w)
+
+   # create the game grid
+   grid.current_tetromino = current_tetromino
+
+
+   # the main game loop (keyboard interaction for moving the tetromino) 
+   while True:
+     
+      # check user interactions via the keyboard
+      
+      if stddraw.hasNextKeyTyped():  # check if the user has pressed a key
+         key_typed = stddraw.nextKeyTyped()  # the most recently pressed key
+
+         if key_typed == "p":
+      
+            paused = not paused
+               
+         elif paused is False:
+         # if the left arrow key has been pressed
+            if key_typed == "left":
+               # move the active tetromino left by one
+               current_tetromino.move(key_typed, grid) 
+            # if the right arrow key has been pressed
+            elif key_typed == "right":
+               # move the active tetromino right by one
+               current_tetromino.move(key_typed, grid)
+            # if the down arrow key has been pressed
+            elif key_typed == "down":
+               # move the active tetromino down by one 
+               # (soft drop: causes the tetromino to fall down faster)
+               current_tetromino.move(key_typed, grid)
+            # clear the queue of the pressed keys for a smoother interaction
+            elif key_typed == "up":
+               current_tetromino.rotate(current_tetromino)
+
+            elif key_typed == "space":# # Piece Drop
+               for i in range(grid_h):
+                  current_tetromino.move("down",grid)
+
+                  
+            elif key_typed=='r':
+               start(grid_h, grid_w, main_w,paused, game_over, restart)
+               
+
+
+         stddraw.clearKeysTyped()
+
+      # move the active tetromino down by one at each iteration (auto fall)
+      if paused is False:
+         success = current_tetromino.move("down", grid)
+
+      # place the active tetromino on the grid when it cannot go down anymore
+      if not success:
+         # get the tile matrix of the tetromino without empty rows and columns
+         # and the position of the bottom left cell in this matrix
+         tiles, pos = grid.current_tetromino.get_min_bounded_tile_matrix(True)
+         # update the game grid by locking the tiles of the landed tetromino
+         game_over = grid.update_grid(tiles, pos)
+         # end the main game loop if the game is over
+         if game_over:
+            gameOver(grid_h, main_w)
+            start(grid_h, grid_w, main_w,paused, game_over, restart)
+
+
+         # create the next tetromino to enter the game grid
+         # by using the create_tetromino function defined below
+         # current_tetromino = create_tetromino(grid_h, grid_w)
+         index_arr, i = grid.checkFullRows(grid_h)
+         grid.deleteRow_and_moveRowDown(grid_h, grid_w, index_arr, i)
+
+         current_tetromino = grid.next_tetromino
+         grid.current_tetromino = current_tetromino
+         next_tetromino = create_tetromino(grid_h,grid_w)
+         grid.next_tetromino = next_tetromino
+         next_tetromino.draw_next_tetromino()
+
+      if restart == True:
+         start(grid_h, grid_w, main_w,paused, game_over, restart)
+
+
+   
+      # display the game grid and the current tetromino
+
+      #grid.merge(grid_h, grid_w)
+      grid.delete_tile(grid_h, grid_w)
+      grid.display(paused)
+
+   print("Game over")
+   restart = False
+   game_over = True
+ 
+
+# Function for creating random shaped tetrominoes to enter the game grid
+def create_tetromino(grid_height, grid_width):
+   # type (shape) of the tetromino is determined randomly
+   tetromino_types = [ 'I', 'O', 'Z', 'L', 'S', 'T', 'J' ]
+   random_index = random.randint(0, len(tetromino_types) - 1)
+   random_type = tetromino_types[random_index]
+   # create and return the tetromino
+   tetromino = Tetromino(random_type, grid_height, grid_width)
+   
+   return tetromino
+
+# Function for displaying a simple menu before starting the game
+def display_game_menu(grid_height, grid_width,paused, game_over, restart):
+   # colors used for the menu
+   background_color = Color(42, 69, 99)
+   button_color = Color(25, 255, 228)
+   text_color = Color(31, 160, 239)
+   # clear the background canvas to background_color
+   stddraw.clear(background_color)
+   # get the directory in which this python code file is placed
+   current_dir = os.path.dirname(os.path.realpath(__file__))
+   # path of the image file
+   img_file = current_dir + "/images/menu_image.png"
+   # center coordinates to display the image
+   img_center_x, img_center_y = (grid_width - 1) / 2, grid_height - 7
+   # image is represented using the Picture class
+   image_to_display = Picture(img_file)
+   # display the image
+   stddraw.picture(image_to_display, img_center_x, img_center_y)
+   # dimensions of the start game button
+   button_w, button_h = grid_width - 1.5, 2
+   # coordinates of the bottom left corner of the start game button 
+   button_blc_x, button_blc_y = img_center_x - button_w / 2, 4
+   # display the start game button as a filled rectangle
+   stddraw.setPenColor(button_color)
+   stddraw.filledRectangle(button_blc_x, button_blc_y, button_w - 1, button_h)
+   # display the text on the start game button
+   stddraw.setFontFamily("Arial")
+   stddraw.setFontSize(25)
+   stddraw.setPenColor(text_color)
+
+
+      
+      
+   text_to_display = "Click Here to Start the Game"
+   stddraw.text(img_center_x, 5, text_to_display)
+   while True:
+           stddraw.show(50)
+           if stddraw.mousePressed():
+            # get the x and y coordinates of the location at which the mouse has 
+            # most recently been left-clicked  
+            mouse_x, mouse_y = stddraw.mouseX(), stddraw.mouseY()
+            # check if these coordinates are inside the button
+            if mouse_x >= button_blc_x and mouse_x <= button_blc_x + button_w:
+                  # change_speed(background_color, button_color, text_color, img_file, grid, grid_height, grid_width)
+                  if mouse_y >= button_blc_y and mouse_y <= button_blc_y + button_h: 
+                     break # break the loop to end the method and start the game
+
+   play_sound()
+                  
+
+
+def gameOver(grid_height, grid_width):
+
+
+   stddraw.clear(Color(42, 69, 99))
+   current_dir = os.path.dirname(os.path.realpath(__file__))
+   # path of the image file
+   img_file = current_dir + "/images/menu_image.png"
+   img_center_x, img_center_y = (grid_width - 1) / 2, grid_height - 7
+   image_to_display = Picture(img_file)
+   stddraw.picture(image_to_display, img_center_x, img_center_y)
+   button_w, button_h = grid_width - 1.5, 2
+   button_blc_x, button_blc_y = img_center_x - button_w / 2, 4
+
+   stddraw.setPenColor(Color(25, 255, 228))
+   stddraw.filledRectangle(button_blc_x, button_blc_y, button_w, button_h)
+
+   stddraw.setFontFamily("Arial")
+   stddraw.setFontSize(30)
+   stddraw.setPenColor(Color(31, 160, 239))
+   stddraw.text(img_center_x, 9, "Game Over")
+   
+   stddraw.setFontSize(25)
+   stddraw.text(img_center_x, 3, "Score : " + str(grid.score))
+   stddraw.text(img_center_x, 5, "Try Again")
+
+   while True:
+      stddraw.show(50)
+      if stddraw.mousePressed():
+         mouse_x, mouse_y = stddraw.mouseX(), stddraw.mouseY()
+         if mouse_x >= button_blc_x and mouse_x <= button_blc_x + button_w:
+            if mouse_y >= button_blc_y and mouse_y <= button_blc_y + button_h:
+               grid.game_over = False
+               break
+
+
+def play_sound():
+   current_dir = os.path.dirname(os.path.realpath(__file__))
+   music_file = current_dir + "/lib/tetris.mp3"
+   pygame.init()
+   music = pygame.mixer.music.load(os.path.join(music_file))
+   pygame.mixer.music.play()
+    
+
+
+            
+def change_speed(grid_height, grid_width):
+   
+   
+        stddraw.clear(Color(42, 69, 99))
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+        # path of the image file
+        img_file = current_dir + "/images/menu_image.png"
+        # center coordinates to display the image
+        img_center_x, img_center_y = (grid_width - 1) / 2, grid_height - 7
+        # image is represented using the Picture class
+        image_to_display = Picture(img_file)
+        # display the image
+        stddraw.picture(image_to_display, img_center_x, img_center_y)
+        # dimensions of the start game button
+        button_w, button_h = grid_width - 13.5, 2
+        stddraw.setPenColor(Color(25, 255, 228))
+        # coordinates of the bottom left corner of the start game button
+        button1_blc_x, button1_blc_y = img_center_x - button_w / 2, 4
+        button2_blc_x, button2_blc_y = button1_blc_x - 5, 4
+        button3_blc_x, button3_blc_y = button1_blc_x + 5, 4
+
+        stddraw.filledRectangle(button1_blc_x, button1_blc_y, button_w, button_h)
+        stddraw.filledRectangle(button2_blc_x, button2_blc_y, button_w, button_h)
+        stddraw.filledRectangle(button3_blc_x, button3_blc_y, button_w, button_h)
+        stddraw.setPenColor(Color(25, 255, 228))
+        
+        stddraw.setFontFamily("Arial")
+        stddraw.setFontSize(25)
+        stddraw.setPenColor(Color(31, 160, 239))
+
+        stddraw.text(img_center_x - 5, 5, "I'm too young to die")
+        stddraw.text(img_center_x, 5, "Hurt me plenty")
+        stddraw.text(img_center_x + 5, 5, "Nightmare!")
+
+        while True:
+            # display the menu and wait for a short time (50 ms)
+            stddraw.show(50)
+            # check if the mouse has been left-clicked
+            if stddraw.mousePressed():
+
+                mouse_x, mouse_y = stddraw.mouseX(), stddraw.mouseY()
+
+                if mouse_x >= button1_blc_x and mouse_x <= button1_blc_x + button_w:
+                    if mouse_y >= button1_blc_y and mouse_y <= button1_blc_y + button_h:
+                        grid.normal_speed = 150
+                        break
+
+                if mouse_x >= button2_blc_x and mouse_x <= button2_blc_x + button_w:
+                    if mouse_y >= button2_blc_y and mouse_y <= button2_blc_y + button_h:
+                        grid.normal_speed = 250
+                        break
+                    
+                if mouse_x >= button3_blc_x and mouse_x <= button3_blc_x + button_w:
+                    if mouse_y >= button3_blc_y and mouse_y <= button3_blc_y + button_h:
+                        grid.normal_speed = 100 
+                        break
+
+
+# start() function is specified as the entry point (main function) from which 
+# the program starts execution
+if __name__== '__main__':
+   draw_canvas()
+   start(grid_h, grid_w, main_w, paused, game_over, restart)
---
+++ game_grid.py
@@ -0,0 +1,241 @@
+import lib.stddraw as stddraw  # stddraw is used as a basic graphics library
+from lib.color import Color # used for coloring the game grid
+from point import Point  # used for tile positions
+import numpy as np  # fundamental Python module for scientific computing
+from tile import Tile
+from tetromino import Tetromino
+
+# Class used for modelling the game grid
+class GameGrid:
+	# Constructor for creating the game grid based on the given arguments
+   def __init__(self, grid_h, grid_w):
+      # set the dimensions of the game grid as the given arguments
+      self.grid_height = grid_h
+      self.grid_width = grid_w
+      # create a tile matrix to store the tiles landed onto the game grid
+      self.tile_matrix = np.full((grid_h, grid_w), None)
+      # create the tetromino that is currently being moved on the game grid
+      self.current_tetromino = None
+      self.next_tetromino = None
+      # the game_over flag shows whether the game is over or not
+      self.game_over = False
+      # set the color used for the empty grid cells
+      self.empty_cell_color = Color(42, 69, 99)
+      # set the colors used for the grid lines and the grid boundaries
+      self.line_color = Color(0, 100, 200) 
+      self.boundary_color = Color(0, 100, 200)
+      # thickness values used for the grid lines and the boundaries
+      self.line_thickness = 0.002
+      self.box_thickness = 10 * self.line_thickness
+      self.normal_speed = 250
+
+      self.paussed = False
+      self.score = 0
+      self.restart = False
+
+   # Method used for displaying the game grid
+   def display(self, paussed):
+
+      # clear the background to empty_cell_color
+      stddraw.clear(self.empty_cell_color)
+      # draw the game grid
+
+      self.draw_grid(paussed)
+      # draw the current/active tetromino if it is not None (the case when the 
+      # game grid is updated)
+      if self.current_tetromino is not None:
+         self.current_tetromino.draw()
+
+      # draw a box around the game grid 
+      self.draw_boundaries()
+      self.next_tetromino.draw_next_tetromino()
+      self.merge(self.grid_height, self.grid_width)
+
+      # show the resulting drawing with a pause duration = 250 ms
+      # stddraw.show(250)
+      stddraw.show(self.normal_speed)  
+
+
+
+   def merge(self,grid_h, grid_w):
+        
+        i , j = 0, 0
+        while(i < grid_h):
+            while(j < grid_w):
+
+                if self.tile_matrix[i][j] != None and self.tile_matrix[i + 1][j] != None:
+                    if self.tile_matrix[i][j].tile_number == self.tile_matrix[i + 1][j].tile_number:
+
+                        self.tile_matrix[i + 1][j] = None
+                        self.tile_matrix[i][j].tile_number += self.tile_matrix[i][j].tile_number
+                        self.score += self.tile_matrix[i][j].tile_number
+                        self.tile_matrix[i][j].decide_color(self.tile_matrix[i][j].tile_number)
+                        i = 0
+                        j = 0
+                j += 1
+            i += 1
+            j = 0
+
+    
+
+   def checkFullRows(self, grid_h):
+
+      index_arr = []
+
+      for i in range(grid_h):
+         if self.checkEmptyOrNot(i):
+            index_arr.append(i)
+            for j in range(self.grid_width):
+               self.score += self.tile_matrix[i][j].tile_number
+
+      return index_arr, i
+      
+      
+   def checkEmptyOrNot(self, i):
+      if None not in self.tile_matrix[i]:
+         return True
+      else:
+         return False
+
+
+   def deleteRow_and_moveRowDown(self, grid_h, grid_w, index_arr, index):
+       
+       for i in index_arr:
+            self.tile_matrix = np.delete(self.tile_matrix, i, axis=0)
+            self.tile_matrix = np.append(self.tile_matrix, np.full((1, grid_w), None), axis=0)
+            
+            for j in range(i, grid_h):
+               for k in range(grid_w):
+                  if self.tile_matrix[j][k] != None:
+                     self.tile_matrix[j][k].move(0, -1)
+
+
+   def delete_tile(self, grid_h, grid_w):
+
+      for i in range(1,grid_h - 1):   #IndexError: index 12 is out of bounds for axis 0 with size 12
+         for j in range(1,grid_w - 1):
+            if self.tile_matrix[i][j] is not None:
+
+               if self.tile_matrix[i + 1][j] == None and self.tile_matrix[i][j + 1] == None and self.tile_matrix[i - 1][j] == None and self.tile_matrix[i][j - 1] == None:
+                
+                  self.score += self.tile_matrix[i][j].tile_number
+                  self.tile_matrix[i][j] = None
+                  self.delete_tile(grid_h, grid_w)
+
+    
+
+   # Method for drawing the cells and the lines of the game grid
+   def draw_grid(self, paussed):
+      # for each cell of the game grid
+      for row in range(self.grid_height):
+         for col in range(self.grid_width):
+            # draw the tile if the grid cell is occupied by a tile
+            if self.tile_matrix[row][col] is not None:
+               self.tile_matrix[row][col].draw(Point(col, row))
+               # self.tile_matrix[row][col].draw()
+
+
+
+      # Draws the main score on the top right of the main game screen
+      # self.showScore(self.score)
+
+      # Displays total number of count how many times the speed increased
+      self.showInformations(self.score, self.normal_speed, paussed)
+
+      # draw the inner lines of the grid
+      stddraw.setPenColor(self.line_color)
+      stddraw.setPenRadius(self.line_thickness)
+      # x and y ranges for the game grid
+      start_x, end_x = -0.5, self.grid_width - 0.5
+      start_y, end_y = -0.5, self.grid_height - 0.5
+      for x in np.arange(start_x + 1, end_x, 1):  # vertical inner lines
+         stddraw.line(x, start_y, x, end_y)
+      for y in np.arange(start_y + 1, end_y, 1):  # horizontal inner lines
+         stddraw.line(start_x, y, end_x, y)
+      stddraw.setPenRadius()  # reset the pen radius to its default value  
+  
+   def set_next(self, next_tetromino):
+        self.next_tetromino = next_tetromino
+
+   def showInformations(self, score, speed, paussed):
+
+        stddraw.setFontSize(18)
+        stddraw.setPenColor(Color(255, 255, 255))
+
+        text_to_display = "Score: " + str(score)
+        stddraw.text(14.4, 18.8, text_to_display) 
+        
+        text_to_display = "Press p to pauss the game"
+        stddraw.text(14.4, 16.5, text_to_display)
+        
+        text_to_display = "Press r to restart the game"
+        stddraw.text(14.4, 15.5, text_to_display)  
+        
+        text_to_display = "Game speed : " + str(speed)
+        stddraw.text(14.4, 12.5, text_to_display)  
+        
+        text_to_display = "NEXT TETROMINO"
+        stddraw.text(14.4, 10.5, text_to_display)
+
+        if paussed is True:
+           stddraw.setFontSize(30)
+           stddraw.text(6, 15, "Game is Paused")
+
+
+      
+   # Method for drawing the boundaries around the game grid 
+   def draw_boundaries(self):
+      # draw a bounding box around the game grid as a rectangle
+      stddraw.setPenColor(self.boundary_color)  # using boundary_color
+      # set the pen radius as box_thickness (half of this thickness is visible 
+      # for the bounding box as its lines lie on the boundaries of the canvas)
+      stddraw.setPenRadius(self.box_thickness)
+      # the coordinates of the bottom left corner of the game grid
+      pos_x, pos_y = -0.5, -0.5
+      stddraw.rectangle(pos_x, pos_y, self.grid_width, self.grid_height)
+      stddraw.setPenRadius()  # reset the pen radius to its default value
+
+   # Method used for checking whether the grid cell with given row and column 
+   # indexes is occupied by a tile or empty
+   def is_occupied(self, row, col):
+      # considering newly entered tetrominoes to the game grid that may have 
+      # tiles with position.y >= grid_height
+      if not self.is_inside(row, col):
+         return False
+      # the cell is occupied by a tile if it is not None
+      return self.tile_matrix[row][col] is not None
+      
+   # Method used for checking whether the cell with given row and column indexes 
+   # is inside the game grid or not
+   def is_inside(self, row, col):
+      if row < 0 or row >= self.grid_height:
+         return False
+      if col < 0 or col >= self.grid_width:
+         return False
+      return True
+
+   # Method that locks the tiles of the landed tetromino on the game grid while
+   # checking if the game is over due to having tiles above the topmost grid row.
+   # The method returns True when the game is over and False otherwise.
+   def update_grid(self, tiles_to_lock, blc_position):
+      # necessary for the display method to stop displaying the tetromino
+      self.current_tetromino = None
+      # lock the tiles of the current tetromino (tiles_to_lock) on the game grid 
+      n_rows, n_cols = len(tiles_to_lock), len(tiles_to_lock[0])
+      for col in range(n_cols):
+         for row in range(n_rows):            
+            # place each tile onto the game grid
+            if tiles_to_lock[row][col] is not None:
+               # compute the position of the tile on the game grid
+               pos = Point()
+               pos.x = blc_position.x + col
+               pos.y = blc_position.y + (n_rows - 1) - row
+               if self.is_inside(pos.y, pos.x):
+                  self.tile_matrix[pos.y][pos.x] = tiles_to_lock[row][col]
+               # the game is over if any placed tile is above the game grid
+               else:
+                  self.game_over = True
+
+   
+      return self.game_over
+
